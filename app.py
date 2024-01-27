7996beed9e90f0cb7fc44ca5e05918a3a29229be
H.SOULB <ha.soulb@gmail.com>
2024-01-27 16:31:21 +0100

    second commit

--- app.py
+++ app.py
@@ -8,18 +8,29 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
+server = '172.16.15.69'
+database = 'SIHRDV'
+username = 'sa'
+password = 'sa2014as'
 
 
-
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def ListRDV():
-    server = '172.16.15.69'
-    database = 'TESTMEDIC'
-    username = 'sa'
-    password = 'sa2014as'
-    conn = pyodbc.connect(
-        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    cur= conn.execute("SELECT * FROM medic")
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur= conn.execute("SELECT * FROM RDV")
+    patients = cur.fetchall()
+    print(patients)
+    if request.method == "POST":
+        data = dict(request.form)
+        patients = getRDV(data["search"])
+    else:
+        print("Aucun résultat")
+    return render_template('RDV.html', patients=patients)
+
+@app.route('/rdv')
+def getRDV(search):
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur = conn.execute("SELECT * FROM RDV  WHERE EXAMEN  LIKE ? OR SERVICE   LIKE ?", ("%"+search+"%", "%"+search+"%",))
     patients = cur.fetchall()
     print(patients)
     return render_template('RDV.html', patients=patients)
